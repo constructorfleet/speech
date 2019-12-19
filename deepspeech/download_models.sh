233cca6e4c584697fc08ac2be07507ff0ffd846a
Teagan Glenn <teagan.m.glenn@gmail.com>
2019-12-19 01:58:06 -0700

    Download script to speed up build

---
+++ deepspeech/download_models.sh
@@ -0,0 +1,9 @@
+cd models
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.1.1/audio-0.1.1.tar.gz
+tar -zxvf audio-0.1.1.tar.gz
+rm audio-0.1.1.tar.gz
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.1.1/deepspeech-0.1.1-models.tar.gz
+tar -zxvf deepspeech-0.1.1-models.tar.gz
+rm deepspeech-0.1.1-models.tar.gz
+
+cd ..
